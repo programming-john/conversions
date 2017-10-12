d1e0b3de730d2c73f4ca210f55b8a3d2ccbb8800
programming-john <31251360+programming-john@users.noreply.github.com>
2017-10-12 09:24:10 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,17 +6,6 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/p1")
-def render_page1():
-    return render_template('canadatounited.html')
-
-@app.route("/p2")
-def render_page2():
-    return render_template('dollartoeuro.html')
-
-@app.route("/p3")
-def render_page3():
-    return render_template('eurotodollar.html')
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
