640bfff5af6267735fbb30ea1ee7a830c35f1434
programming-john <31251360+programming-john@users.noreply.github.com>
2017-10-12 09:13:08 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,6 +2,10 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
 @app.route("/p1")
 def render_page1():
     return render_template('canadatounited.html')
